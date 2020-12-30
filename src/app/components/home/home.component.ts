d82c554139a70fc255ec15f13c61bd00ae7e58d0
japc <japc.grafico@gmail.com>
2020-12-30 18:32:44 +0100

    Catching the error in Home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,17 +11,23 @@ import { SpotifyService } from '../../services/spotify.service';
 export class HomeComponent implements OnInit {
 
   newSongs: any[] = [];
-
   loading: boolean;
+  error: boolean;
+  msgError: string;
 
   constructor(private spotifyService: SpotifyService) {
-
+    this.error = false;
     this.loading = true;
 
     this.spotifyService.getNewReleases()
       .subscribe( (data: any) => {
         this.newSongs = data;
         this.loading = false;
+      }, (error: any ) => {
+        // console.log(error);
+        this.loading = false;
+        this.error = true;
+        this.msgError = error.error.error.message;
       });
   }
 
